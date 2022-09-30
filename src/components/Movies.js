e47963bf3f1853f5863d3b153f4aee934cd2e99c
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-09-30 21:13:16 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,42 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    <div>
+      <h2>{movies[0].title}</h2>
+      <h3>{movies[0].time}</h3>
+      <ul>
+        <li>{movies[0].genres[0]}</li>
+        <li>{movies[0].genres[1]}</li>
+        <li>{movies[0].genres[2]}</li>
+        <li>{movies[0].genres[3]}</li>
+        <li>{movies[0].genres[4]}</li>
+      </ul>
+    </div>
+    <div>
+    <h2>{movies[1].title}</h2>
+      <h3>{movies[1].time}</h3>
+      <ul>
+        <li>{movies[1].genres[0]}</li>
+        <li>{movies[1].genres[1]}</li>
+        <li>{movies[1].genres[2]}</li>
+        <li>{movies[1].genres[3]}</li>
+        <li>{movies[1].genres[4]}</li>
+      </ul>
+    </div>
+    <div>
+    <h2>{movies[2].title}</h2>
+      <h3>{movies[2].time}</h3>
+      <ul>
+        <li>{movies[2].genres[0]}</li>
+        <li>{movies[2].genres[1]}</li>
+        <li>{movies[2].genres[2]}</li>
+        <li>{movies[2].genres[3]}</li>
+        <li>{movies[2].genres[4]}</li>
+      </ul>
+    </div>
+  </div>;
 }
 
 export default Movies;
